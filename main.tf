c90ce738fc4e2d74255d84dd15601c7caf6e4f0a
Gabriela Barboza <barbozagabis@gmail.com>
2024-05-03 00:43:16 +0000

    Add: Fix TF vpc log vul

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-### CREATE VPC ###
-
+### CREATE VPC ##
 resource "aws_vpc" "vpc_network" {
   cidr_block       = var.vpc_range
   instance_tenancy = "default"
@@ -8,8 +7,32 @@ resource "aws_vpc" "vpc_network" {
   }
 }
 
-### CREATE SUBNET PUBLIC E PRIVADA 1A ##
+### ADICIONANDO LOG PARA VPC ###
+resource "aws_flow_log" "flow_log" {
+  log_destination      = "arn:aws:s3:::gabs-clc11-tfstatee"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.minha_vpc.id
+}
 
+### ADICIONANDO SECURITY GROUP SEPARADAMENTE ###
+#resource "aws_default_security_group" "default" {
+#  vpc_id = aws_vpc.issue_vpc.id
+#  ingress {
+#    protocol  = "-1"
+#    self      = true
+#    from_port = 0
+#    to_port   = 0
+#  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = var.vpc_range
+#  }
+#}
+
+### CREATE SUBNET PUBLIC E PRIVADA 1A ##
 resource "aws_subnet" "subnet_privada_1a" {
   vpc_id            = aws_vpc.vpc_network.id
   cidr_block        = var.subnet_privada_1a_range
@@ -29,7 +52,6 @@ resource "aws_subnet" "subnet_publica_1a" {
 }
 
 ### CREATE SUBNET PUBLIC E PRIVADA 1C ##
-
 resource "aws_subnet" "subnet_privada_1c" {
   vpc_id            = aws_vpc.vpc_network.id
   cidr_block        = var.subnet_privada_1c_range
@@ -49,7 +71,6 @@ resource "aws_subnet" "subnet_publica_1c" {
 }
 
 ### CREATE INTERNET GATEWAY ### 
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.vpc_network.id
 
@@ -59,7 +80,6 @@ resource "aws_internet_gateway" "gw" {
 }
 
 ### CREATE ROUTE TABLE ### 
-
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vpc_network.id
 
@@ -73,7 +93,6 @@ resource "aws_route_table" "public_rt" {
 }
 
 ### ASSOCIATION ROUTE TABLE ### 
-
 resource "aws_route_table_association" "public-1a" {
   subnet_id      = aws_subnet.subnet_publica_1a.id
   route_table_id = aws_route_table.public_rt.id
@@ -86,13 +105,11 @@ resource "aws_route_table_association" "public-1c" {
 
 
 ### CREATE IP PUBLICO 1A ###
-
 resource "aws_eip" "ip_nat_1a"{
     domain = "vpc"
 }
 
 ### NAT GATEWAY 1A ###
- 
 resource "aws_nat_gateway" "natgateway_1a" {
   allocation_id = aws_eip.ip_nat_1a.id
   subnet_id     = aws_subnet.subnet_publica_1a.id
@@ -103,13 +120,11 @@ resource "aws_nat_gateway" "natgateway_1a" {
 }
   
 ### CREATE IP PUBLICO 1C ###
-
 resource "aws_eip" "ip_nat_1c"{
     domain = "vpc"
 } 
 
 ### NAT GATEWAY 1C ###
-
 resource "aws_nat_gateway" "natgateway_1c" {
   allocation_id = aws_eip.ip_nat_1c.id
   subnet_id     = aws_subnet.subnet_publica_1c.id
@@ -120,7 +135,6 @@ resource "aws_nat_gateway" "natgateway_1c" {
 }
 
 ### CREATE ROUTE TABLE PRIVADA 1A ###
-
 resource "aws_route_table" "private_rt_1a" {
   vpc_id = aws_vpc.vpc_network.id
 
@@ -134,7 +148,6 @@ resource "aws_route_table" "private_rt_1a" {
 }
 
 ### CREATE ROUTE TABLE PUBLICA 1C ###
-
 resource "aws_route_table" "private_rt_1c" {
   vpc_id = aws_vpc.vpc_network.id
 
@@ -148,7 +161,6 @@ resource "aws_route_table" "private_rt_1c" {
 }
 
 ### ASSOCIATION ROUTE TABLE PRIVADA ### 
-
 resource "aws_route_table_association" "private-1a" {
   subnet_id      = aws_subnet.subnet_privada_1a.id
   route_table_id = aws_route_table.private_rt_1a.id
